cc3cc0d1956d815c246560d6a4f565ca2c2497ee
ZygarM251 <115568775+ZygarM251@users.noreply.github.com>
2025-06-12 13:04:02 +0200

    Add files via upload

--- Zadanie4W1/src/app/lista/lista.component.ts
+++ Zadanie4W1/src/app/lista/lista.component.ts
@@ -4,18 +4,26 @@ import { Observable } from 'rxjs';
 import { Ksiazka } from '../../models/ksiazka';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista',
-  imports: [CommonModule, RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.css'
 })
 export class ListaComponent {
   private readonly listaService = inject(ListaService);
-  public dane$: Observable<Ksiazka[]>;
+
+  public dane: Ksiazka[] = [];
+  public fraza: string = '';
 
   constructor() {
-    this.dane$ = this.listaService.get();
+    this.listaService.get().subscribe(d => this.dane = d);
+  }
+  
+  filtruj() {
+    this.listaService.get(this.fraza).subscribe(d => this.dane = d);
   }
 }
